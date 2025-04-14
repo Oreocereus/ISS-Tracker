4780cfe8288580f0404ec6b43ede8e6ef084b110
Wesley <125459384+Oreocereus@users.noreply.github.com>
2025-04-14 11:36:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"]) + 16
-iss_longitude = float(data["iss_position"]["longitude"]) + 90
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
 
 parameters = {
     "lat": MY_LAT,
